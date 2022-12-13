223e708035f10ac9387ec27caa2d3acc1f700120
corvo22 <kdm2013red@gmail.com>
2022-12-13 17:13:40 -0500

    We can scrape, we can parse

---
+++ scrape.py
@@ -0,0 +1,83 @@
+import re
+import requests
+import math
+import random
+from bs4 import BeautifulSoup
+from datetime import date
+
+def getSiteData():
+	# main driver. gets the site data using an html request. we then feed to process.
+
+	current_date = getDate()
+	requestor = requests.get(current_date)
+
+	# code 200 means a successful connection an dreturn response from the page. if this fails we cannot go forward.
+	if(requestor.status_code != 200):
+		raise Exception("error creating a connection: Code " + requestor.status_code)
+		
+	processData(requestor)
+
+def getDate():
+	today = date.today()
+	date_string = today.strftime("%B_%d")
+
+	return "https://en.wikipedia.org/wiki/" + date_string
+
+def getRandomEvents(event_list):
+	# returns a number of events from the current section
+
+	number_of_events = math.ceil(len(event_list) / 5)
+	random_events = []
+
+	for i in range(number_of_events):
+		rand = random.randint(0, len(event_list) - 1)
+		event = event_list[rand]
+		event_list.pop(rand)
+		random_events.append(event)
+
+	return random_events
+
+def processSection(event_string):
+	# function to process wikipedia section, ie events, births, deaths, etc
+	# takes the inputted string and splits into three lists for each subsection, pre1600, early modern period, and modern period
+	# lists comprise of each li element from the original html in plain text form.
+	events_chosen = []
+	event_string = re.sub("\[[0-9]+\]", "", event_string)
+	eras = event_string.split("[edit]")
+	
+	# converting raw data into list of events broken down by respective header from wikipedia page
+	# -1 to ignore headers, list comp to remove "" members
+
+	pre1600 = eras[1].split("\n")[:-1]
+	pre1600 = [i for i in pre1600 if i] 
+	
+	early_modern = eras[2].split("\n")[:-1]
+	early_modern = [i for i in early_modern if i]
+
+	modern = eras[3].split("\n")[:-1]
+	modern = [i for i in modern if i]
+
+	events_chosen = events_chosen + getRandomEvents(pre1600) + getRandomEvents(early_modern) + getRandomEvents(modern)
+	print(events_chosen)
+
+def processData(requestor):
+	# Beautiful soup is an html parser/interpreter. We use that to pull all text out of the site 
+	# and then check it into the sections we want from word x to word y where x and y are headers
+
+	soup = BeautifulSoup(requestor.content, 'html.parser')
+	text = soup.get_text()
+
+	events = text.split("Events[edit]")[1].split("Births[edit]")[0]
+	births = text.split("Births[edit]")[1].split("Deaths[edit]")[0]
+
+	processSection(events)
+
+
+
+
+
+def main():
+	getSiteData()
+
+if __name__ == "__main__":
+	main()
